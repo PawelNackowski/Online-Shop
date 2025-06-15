a1780c9950cfee3ab219c9e3960af1beb22b0da0
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-15 21:11:11 +0200

    styled navigation

---
+++ src/Navigation/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ListContainer = styled.nav`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 20px;
+`
+export const ListItem = styled.li`
+    list-style: none;
+    margin: 0 15px;
+`
+
+export const Link = styled.a`
+    text-decoration: none;
+    
+`
