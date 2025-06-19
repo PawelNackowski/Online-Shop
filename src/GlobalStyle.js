8a6d9437fc1881592e97d61d05462f9b161cb09a
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-19 17:36:55 +0200

    created global style

---
+++ src/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    html, body, #root {
+        height: 100%;
+    }
+
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+    body {
+        text-align: center;
+        color: white;
+        background: #050069;
+        background: linear-gradient(0deg, rgb(4, 0, 74) 0%, rgba(1, 1, 140, 1) 61%, rgb(0, 121, 145) 100%);
+    }
+`;
+export default GlobalStyle;
