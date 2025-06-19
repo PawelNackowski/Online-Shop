0c491f9636e2b80915d3161c10566af98377964b
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-19 17:37:07 +0200

    added global style

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyle from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <>
+        <GlobalStyle/>
+        <App />
+    </>
 );
 
 reportWebVitals();
