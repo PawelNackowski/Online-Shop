24fbfd1a9dc83da2cd726e16426493c7921d693c
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-19 17:36:35 +0200

    added header component and pages routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import { Navigation } from "./Navigation/Navigation";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  toAbout,
+  toContact,
+  toHome,
+  toLogin,
+  toProductDetails,
+  toProducts,
+  toRegister,
+} from "./common/Header/Navigation/routes";
+import { Page } from "./common/Page";
+import { PageHome } from "./features/PageHome";
+import { PageProducts } from "./features/PageProducts";
+import { PageAbout } from "./features/PageAbout";
+import { PageContact } from "./features/PageContact";
+import { Header } from "./common/Header";
 
 function App() {
   return (
     <>
-    <h1>Online Shop</h1>
-    <Navigation />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={toHome()} element={<Page body={<PageHome />} />} />
+          <Route
+            path={toProducts()}
+            element={<Page body={<PageProducts />} />}
+          />
+          <Route path={toAbout()} element={<Page body={<PageAbout />} />} />
+          <Route path={toContact()} element={<Page body={<PageContact />} />} />
+          <Route
+            path={toLogin()}
+            element={<Page body={<div>Login Page</div>} />}
+          />
+          <Route
+            path={toRegister()}
+            element={<Page body={<div>Register Page</div>} />}
+          />
+          <Route
+            path={toProductDetails(":productId")}
+            element={<Page body={<div>Product Details Page</div>} />}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
