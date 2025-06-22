875cb146cac1b54ab2003b94438c7b5d4ac0ebfc
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-22 22:13:22 +0200

    creating a preliminary form style

---
+++ src/features/LoginPage/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #ccc;
+  padding: 20px;
+`;
+
+export const StyledForm = styled.form`
+
+`;
+
+export const WrapperForm = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
