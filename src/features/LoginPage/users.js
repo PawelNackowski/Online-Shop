e7c33544e45c7f4f422f6559010df26a14fc4eb2
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-22 22:14:01 +0200

    created test users table

---
+++ src/features/LoginPage/users.js
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    id: 1,
+    nickname: "admin",
+    username: "admin@test.com",
+    password: "admin",
+  },
+  {
+    id: 2,
+    nickname: "user",
+    username: "user@test.com",
+    password: "user",
+  },
+]
