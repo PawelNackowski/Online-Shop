00213b2e462790bd54949a587105b58636d40951
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-24 14:48:47 +0200

    added if login user then show user name

--- src/common/Header/LoginButton/index.js
+++ src/common/Header/LoginButton/index.js
@@ -1,16 +1,25 @@
+import { useAuth } from "../../../AuthContext";
 import { StyledLink, StyledList, StyledNavigation } from "./styled";
 
 export const LoginButton = () => {
+  const {user} = useAuth();
+
   return (
     <StyledNavigation>
       <StyledList>
-        <li>
+        {user ? (
+          <li>Hello {user.nickname}</li>
+        ) : (
+          <>
+          <li>
           <StyledLink to="/login">Login</StyledLink>
         </li>
         /
         <li>
           <StyledLink to="/register">Register</StyledLink>
         </li>
+        </>
+        )}
       </StyledList>
     </StyledNavigation>
   );
