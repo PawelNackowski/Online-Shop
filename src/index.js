dd3829f974f523d8e17fd9ec25979f76a5cdc5ee
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-24 14:47:25 +0200

    added auth provider to app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import GlobalStyle from './GlobalStyle';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "./GlobalStyle";
+import { AuthProvider } from "./AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <>
-        <GlobalStyle/>
-        <App />
-    </>
+  <>
+    <AuthProvider>
+      <GlobalStyle />
+      <App />
+    </AuthProvider>
+  </>
 );
 
 reportWebVitals();
