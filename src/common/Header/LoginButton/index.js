a922f5fbd532734817f06d70bf34d63b23fad69d
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-24 14:57:47 +0200

    added logout

--- src/common/Header/LoginButton/index.js
+++ src/common/Header/LoginButton/index.js
@@ -2,23 +2,31 @@ import { useAuth } from "../../../AuthContext";
 import { StyledLink, StyledList, StyledNavigation } from "./styled";
 
 export const LoginButton = () => {
-  const {user} = useAuth();
+  const { user, setUser } = useAuth();
+
+  const handleLogout = () => {
+    setUser(null);
+    alert("You have been logged out.");
+  }
 
   return (
     <StyledNavigation>
       <StyledList>
         {user ? (
-          <li>Hello {user.nickname}</li>
+          <>
+            <li>Hello {user.nickname}</li>
+            <StyledLink onClick={handleLogout}>Logout</StyledLink>
+          </>
         ) : (
           <>
-          <li>
-          <StyledLink to="/login">Login</StyledLink>
-        </li>
-        /
-        <li>
-          <StyledLink to="/register">Register</StyledLink>
-        </li>
-        </>
+            <li>
+              <StyledLink to="/login">Login</StyledLink>
+            </li>
+            /
+            <li>
+              <StyledLink to="/register">Register</StyledLink>
+            </li>
+          </>
         )}
       </StyledList>
     </StyledNavigation>
