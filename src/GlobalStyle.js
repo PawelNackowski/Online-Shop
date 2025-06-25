f4e335d37767470109447c183bd5d327071c5c11
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-25 21:53:37 +0200

    added nim-height

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    html, body, #root {
-        height: 100%;
-    }
 
     html {
         box-sizing: border-box;
@@ -13,9 +10,11 @@ const GlobalStyle = createGlobalStyle`
         box-sizing: inherit;
     }
     body {
+        min-height: 100vh;
         text-align: center;
         font-family: 'Open Sans', sans-serif;
         color: white;
+        background-repeat: no-repeat;
         background: #050069;
         background: linear-gradient(0deg, rgb(4, 0, 74) 0%, rgba(1, 1, 140, 1) 61%, rgb(0, 121, 145) 100%);
     }
