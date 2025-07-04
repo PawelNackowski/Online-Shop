c46609baad5c4feb7988e76eb88579b41e85bfc6
Pawel Nackowski <pawelnackowski@gmail.com>
2025-07-04 14:52:31 +0200

    created hidden show password icon

---
+++ src/features/HiddenShowPasswordIcon/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { ReactComponent as IconShow } from "./eye-password-show.svg";
+import { ReactComponent as IconHidden } from "./eye-password-hide.svg";
+
+export const StyledIconShow = styled(IconShow)`
+  width: 24px;
+  height: 24px;
+  `
+export const StyledIconHidden = styled(IconHidden)`
+  width: 24px;
+  height: 24px;
+`
