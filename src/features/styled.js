938b4186057fecdc45d47e26851b7e974cfa6510
Pawel Nackowski <pawelnackowski@gmail.com>
2025-07-04 14:54:00 +0200

    relocated styled icon file up

---
+++ src/features/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Icon = styled.span`
+  position: relative;
+  width: 24px;
+  height: 24px;
+  right: -80%;
+  top: -44px;
+  color: rgb(0, 0, 0);
+  cursor: pointer;
+`;
