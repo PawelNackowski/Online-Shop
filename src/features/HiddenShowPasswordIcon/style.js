18c9071cf82282e543187c9c9a475de95517d2d6
Pawel Nackowski <pawelnackowski@gmail.com>
2025-07-04 20:19:19 +0200

    changed formatting

--- src/features/HiddenShowPasswordIcon/style.js
+++ src/features/HiddenShowPasswordIcon/style.js
@@ -5,8 +5,8 @@ import { ReactComponent as IconHidden } from "./eye-password-hide.svg";
 export const StyledIconShow = styled(IconShow)`
   width: 24px;
   height: 24px;
-  `
+`;
 export const StyledIconHidden = styled(IconHidden)`
   width: 24px;
   height: 24px;
-`
+`;
