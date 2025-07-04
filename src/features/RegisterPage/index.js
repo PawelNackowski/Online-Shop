8f92ed50476b0ac3ac134bf293ab4680812d65ce
Pawel Nackowski <pawelnackowski@gmail.com>
2025-07-04 14:56:06 +0200

    added hidden/show icon

--- src/features/RegisterPage/index.js
+++ src/features/RegisterPage/index.js
@@ -13,6 +13,12 @@ import {
 } from "../RegisterPage/styled";
 import { useNavigate } from "react-router-dom";
 import { toLogin } from "../../common/Header/Navigation/routes";
+import { useTogglePasswordVisibility } from "../useTogglePasswordVisibility";
+import { Icon } from "../styled";
+import {
+  StyledIconHidden,
+  StyledIconShow,
+} from "../HiddenShowPasswordIcon/style";
 
 export const PageRegister = () => {
   const [password, setPassword] = useState("");
@@ -20,6 +26,8 @@ export const PageRegister = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const navigate = useNavigate();
+  const { showPassword, togglePasswordVisibility } =
+    useTogglePasswordVisibility();
 
   const onFormSubmit = (event) => {
     event.preventDefault();
@@ -63,14 +71,17 @@ export const PageRegister = () => {
         <WrapperForm>
           <StyledLabel>Password</StyledLabel>
           <StyledInput
-            type="password"
+            type={showPassword ? "text" : "password"}
             value={password}
             onChange={({ target }) => setPassword(target.value)}
             placeholder="Enter your password"
           />
+          <Icon onClick={togglePasswordVisibility}>
+            {showPassword ? <StyledIconShow /> : <StyledIconHidden />}
+          </Icon>
           <StyledLabel>Confirm password</StyledLabel>
           <StyledInput
-            type="password"
+            type={showPassword ? "text" : "password"}
             value={confirmPassword}
             onChange={({ target }) => setConfirmPassword(target.value)}
             placeholder="Confirm your password"
