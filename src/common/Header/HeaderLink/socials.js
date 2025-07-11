c54218d638f1185687f8df1046807e1a678d563f
Pawel Nackowski <pawelnackowski@gmail.com>
2025-07-11 16:26:21 +0200

    added icon props

--- src/common/Header/HeaderLink/socials.js
+++ src/common/Header/HeaderLink/socials.js
@@ -1,31 +1,41 @@
-import {ReactComponent as SearchIcon} from "./icon/search.svg";
-import {ReactComponent as UserIcon} from "./icon/user.svg";
-import {ReactComponent as CartIcon} from "./icon/shoping-cart.svg";
-import {ReactComponent as HeartIcon} from "./icon/hearth.svg";
+import { ReactComponent as SearchIcon } from "./icon/search.svg";
+import { ReactComponent as UserIcon } from "./icon/user.svg";
+import { ReactComponent as CartIcon } from "./icon/shopping-cart.svg";
+import { ReactComponent as HeartIcon } from "./icon/favorite.svg";
 import { toLogin } from "../../../namePlate";
 import { styledIcon } from "./styled";
-
-
 
 export const socials = [
   {
     name: "search",
     Icon: styledIcon(SearchIcon),
     to: "/search",
-  }
-  ,{
+    iconProps: {
+      iconName: "search",
+    },
+  },
+  {
     name: "user",
     Icon: styledIcon(UserIcon),
     to: toLogin(),
-  }
-  ,{
+    iconProps: {
+      iconName: "user",
+    },
+  },
+  {
     name: "favorite",
     Icon: styledIcon(HeartIcon),
     to: "/wishlist",
+    iconProps: {
+      iconName: "favorite",
+    },
   },
-    {
+  {
     name: "cart",
     Icon: styledIcon(CartIcon),
     to: "/cart",
-  }
-]
+    iconProps: {
+      iconName: "cart",
+    },
+  },
+];
