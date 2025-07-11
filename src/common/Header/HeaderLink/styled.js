b27934f2465aa05eb6e49189c491e82de9f94f01
Pawel Nackowski <pawelnackowski@gmail.com>
2025-07-11 16:28:01 +0200

    deleted unnecessary components and added style to styledIcon

--- src/common/Header/HeaderLink/styled.js
+++ src/common/Header/HeaderLink/styled.js
@@ -1,36 +1,47 @@
-
 import styled from "styled-components";
 
-export const StyledNavigation = styled.nav`
-  display: flex;
-  justify-content: flex-end;
-  max-width: 1400px;
-  width: 100%;
-`;
 export const StyledList = styled.ul`
-  padding-top: 56px;
   display: flex;
   gap: 24px;
   flex-wrap: wrap;
   list-style: none;
-  margin: 0;
-  padding-left: 0;
+  margin: 0 18px 6px 0;
 `;
-
-export const StyledLink = styled.a`
-  text-decoration: none;
-  color: ${({ theme }) => theme.colors.dark};
-  padding: 10px;
-
-  &.active {
-    font-weight: bold;
-  }
-`;
-
-export const IconLink = styled.a``;
-
 
 export const styledIcon = (Icon) => styled(Icon)`
   width: 24px;
   height: 24px;
+
+  &:hover {
+    cursor: pointer;
+    fill: ${({ theme }) => theme.colors.blue};
+  }
+  &:active {
+    transform: scale(0.9);
+    transition: transform 0.2s ease-in-out;
+  }
+
+  ${({ iconName, theme }) =>
+    iconName === "favorite"
+      ? `
+    fill: none;
+    stroke: ${theme.colors.dark};
+    &:hover {
+      fill: ${theme.colors.red};
+      stroke: ${theme.colors.red};
+    }
+  `
+      : ` ""
+  `}
+  ${({ iconName, theme }) =>
+    iconName === "cart"
+      ? `
+    fill: none;
+    stroke: ${theme.colors.dark};
+    &:hover {
+      stroke: ${theme.colors.blue};
+    }
+  `
+      : ` ""
+  `}
 `;
